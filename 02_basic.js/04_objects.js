43fe7cc4f1d93c2ac9a6ad81dee8705bb718efe1
Tanmay273301 <145272161+Tanmay273301@users.noreply.github.com>
2024-02-18 18:02:21 +0530

    objects

---
+++ 02_basic.js/04_objects.js
@@ -0,0 +1,48 @@
+//const FacebookUser = new Object()
+const FacebookUser = {}
+
+
+FacebookUser.id = "125abc"
+FacebookUser.name = "Tanmay Tripathi"
+FacebookUser.isLoggedIn = false
+
+//console.log(FacebookUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userFullname: {
+            firstname: "Tanmay",
+            lastname:  "Tripathi"
+        }
+
+    }
+
+}
+
+console.log(regularUser.fullname.userFullname.firstname);
+
+
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+
+const obj2 = {
+    3: "c",
+    4: "d"
+}
+
+//const obj3 = {obj1,obj2}
+//const obj3 = Object.assign({},obj1,obj2)
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
+
+console.log(FacebookUser);
+
+console.log(Object.keys(FacebookUser));  // output =  ['id', 'name', 'isLoggedIn']
+console.log(Object.values(FacebookUser));// output =   (3) ['125abc', 'Tanmay Tripathi', false]
+
+console.log(Object.entries(FacebookUser));
+
+console.log(FacebookUser.hasOwnProperty('isLoggedIn'));// true
