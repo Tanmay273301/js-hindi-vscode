91339b6778b928fedf05f60f6fa2d2500f79f866
Tanmay273301 <145272161+Tanmay273301@users.noreply.github.com>
2024-02-19 19:51:33 +0530

    IIFE

---
+++ 02_basic.js/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    // named IIFE
+    console.log("DB CONNECTED");
+})();
+
+(() => {
+    console.log("DB CONNECTED TWO");
+})();
+
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})("Tanmay")
