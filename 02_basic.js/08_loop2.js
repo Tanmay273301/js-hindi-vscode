a5069596313b3e8054c0bc807c3429dd4b3f3424
Tanmay273301 <145272161+Tanmay273301@users.noreply.github.com>
2024-02-22 21:54:51 +0530

    while loop

---
+++ 02_basic.js/08_loop2.js
@@ -0,0 +1,26 @@
+
+let index = 0
+while (index <= 10) {
+    //console.log(`value of index is: ${index}`);
+    index++;
+
+    
+}
+
+let myArray = ['flash','superman', 'batman']
+let arr = 0
+while(arr < myArray.length)
+{
+    //console.log(`values is:${myArray[arr]}`);
+    arr++;
+}
+
+//do- while loop
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+    
+} while (score <= 10);
